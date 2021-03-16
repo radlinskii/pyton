b0345d3426d8d078e53b6b09e5cd71f291b488ae
radlinskii <radlinskiignacy@gmail.com>
2021-03-16 20:59:53 +0100

    format

--- new.py
+++ new.py
@@ -16,8 +16,6 @@ except:
     # w/e
 
 
-
-
 (train_images, _), (test_images, _) = tf.keras.datasets.mnist.load_data()
 
 # Each MNIST image is originally a vector of 784 integers,
@@ -25,9 +23,11 @@ except:
 # We model each pixel with a Bernoulli distribution in our model,
 # and we statically binarize the dataset.
 
+
 def preprocess_images(images):
     images = images.reshape((images.shape[0], 28, 28, 1)) / 255.
     return np.where(images > .5, 1.0, 0.0).astype('float32')
+
 
 train_images = preprocess_images(train_images)
 test_images = preprocess_images(test_images)
@@ -106,6 +106,7 @@ class CVAE(tf.keras.Model):
 
 optimizer = tf.keras.optimizers.Adam(1e-4)
 
+
 def log_normal_pdf(sample, mean, logvar, raxis=1):
     log2pi = tf.math.log(2. * np.pi)
     return tf.reduce_sum(
@@ -117,7 +118,8 @@ def compute_loss(model, x):
     mean, logvar = model.encode(x)
     z = model.reparameterize(mean, logvar)
     x_logit = model.decode(z)
-    cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(logits=x_logit, labels=x)
+    cross_ent = tf.nn.sigmoid_cross_entropy_with_logits(
+        logits=x_logit, labels=x)
     logpx_z = -tf.reduce_sum(cross_ent, axis=[1, 2, 3])
     logpz = log_normal_pdf(z, 0., 0.)
     logqz_x = log_normal_pdf(z, mean, logvar)
@@ -132,7 +134,7 @@ def train_step(model, x, optimizer):
     update the model's parameters.
     """
     with tf.GradientTape() as tape:
-      loss = compute_loss(model, x)
+        loss = compute_loss(model, x)
     gradients = tape.gradient(loss, model.trainable_variables)
     optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
@@ -148,46 +150,50 @@ random_vector_for_generation = tf.random.normal(
     shape=[num_examples_to_generate, latent_dim])
 model = CVAE(latent_dim)
 
+
 def generate_and_save_images(model, epoch, test_sample):
-  mean, logvar = model.encode(test_sample)
-  z = model.reparameterize(mean, logvar)
-  predictions = model.sample(z)
-  fig = plt.figure(figsize=(4, 4))
+    mean, logvar = model.encode(test_sample)
+    z = model.reparameterize(mean, logvar)
+    predictions = model.sample(z)
+    fig = plt.figure(figsize=(4, 4))
 
-  for i in range(predictions.shape[0]):
-    plt.subplot(4, 4, i + 1)
-    plt.imshow(predictions[i, :, :, 0], cmap='gray')
-    plt.axis('off')
+    for i in range(predictions.shape[0]):
+        plt.subplot(4, 4, i + 1)
+        plt.imshow(predictions[i, :, :, 0], cmap='gray')
+        plt.axis('off')
 
-  # tight_layout minimizes the overlap between 2 sub-plots
-  plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
-  plt.show()
+    # tight_layout minimizes the overlap between 2 sub-plots
+    plt.savefig('image_at_epoch_{:04d}.png'.format(epoch))
+    plt.show()
+
 
 # Pick a sample of the test set for generating output images
 assert batch_size >= num_examples_to_generate
 for test_batch in test_dataset.take(1):
-  test_sample = test_batch[0:num_examples_to_generate, :, :, :]
+    test_sample = test_batch[0:num_examples_to_generate, :, :, :]
 
 
 generate_and_save_images(model, 0, test_sample)
 
 for epoch in range(1, epochs + 1):
-  start_time = time.time()
-  for train_x in train_dataset:
-    train_step(model, train_x, optimizer)
-  end_time = time.time()
+    start_time = time.time()
+    for train_x in train_dataset:
+        train_step(model, train_x, optimizer)
+    end_time = time.time()
 
-  loss = tf.keras.metrics.Mean()
-  for test_x in test_dataset:
-    loss(compute_loss(model, test_x))
-  elbo = -loss.result()
-  display.clear_output(wait=False)
-  print('Epoch: {}, Test set ELBO: {}, time elapse for current epoch: {}'
-        .format(epoch, elbo, end_time - start_time))
-  generate_and_save_images(model, epoch, test_sample)
+    loss = tf.keras.metrics.Mean()
+    for test_x in test_dataset:
+        loss(compute_loss(model, test_x))
+    elbo = -loss.result()
+    display.clear_output(wait=False)
+    print('Epoch: {}, Test set ELBO: {}, time elapse for current epoch: {}'
+          .format(epoch, elbo, end_time - start_time))
+    generate_and_save_images(model, epoch, test_sample)
+
 
 def display_image(epoch_no):
-  return PIL.Image.open('image_at_epoch_{:04d}.png'.format(epoch_no))
+    return PIL.Image.open('image_at_epoch_{:04d}.png'.format(epoch_no))
+
 
 plt.imshow(display_image(epoch))
 plt.axis('off')  # Display images
@@ -207,26 +213,27 @@ plt.axis('off')  # Display images
 
 
 def plot_latent_images(model, n, digit_size=28):
-  """Plots n x n digit images decoded from the latent space."""
+    """Plots n x n digit images decoded from the latent space."""
 
-  norm = tfp.distributions.Normal(0, 1)
-  grid_x = norm.quantile(np.linspace(0.05, 0.95, n))
-  grid_y = norm.quantile(np.linspace(0.05, 0.95, n))
-  image_width = digit_size*n
-  image_height = image_width
-  image = np.zeros((image_height, image_width))
+    norm = tfp.distributions.Normal(0, 1)
+    grid_x = norm.quantile(np.linspace(0.05, 0.95, n))
+    grid_y = norm.quantile(np.linspace(0.05, 0.95, n))
+    image_width = digit_size*n
+    image_height = image_width
+    image = np.zeros((image_height, image_width))
 
-  for i, yi in enumerate(grid_x):
-    for j, xi in enumerate(grid_y):
-      z = np.array([[xi, yi]])
-      x_decoded = model.sample(z)
-      digit = tf.reshape(x_decoded[0], (digit_size, digit_size))
-      image[i * digit_size: (i + 1) * digit_size,
-            j * digit_size: (j + 1) * digit_size] = digit.numpy()
+    for i, yi in enumerate(grid_x):
+        for j, xi in enumerate(grid_y):
+            z = np.array([[xi, yi]])
+            x_decoded = model.sample(z)
+            digit = tf.reshape(x_decoded[0], (digit_size, digit_size))
+            image[i * digit_size: (i + 1) * digit_size,
+                  j * digit_size: (j + 1) * digit_size] = digit.numpy()
 
-  plt.figure(figsize=(10, 10))
-  plt.imshow(image, cmap='Greys_r')
-  plt.axis('Off')
-  plt.show()
+    plt.figure(figsize=(10, 10))
+    plt.imshow(image, cmap='Greys_r')
+    plt.axis('Off')
+    plt.show()
+
 
 plot_latent_images(model, 20)
